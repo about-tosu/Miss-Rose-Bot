368238710f2860e80155fb662fe7993d6506b726
Tosu <167279662+about-tosu@users.noreply.github.com>
2024-12-27 06:46:15 +0530

    Update __init__.py

--- tg_bot/modules/sql/__init__.py
+++ tg_bot/modules/sql/__init__.py
@@ -5,16 +5,14 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 from tg_bot import DB_URI
 
 
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-BASE = declarative_base()
-
-def start():
-    engine = create_engine("postgres://rgywlsqe:lSnciWStimBbJNt5d7EKltRLvFBmAzgA@tyke.db.elephantsql.com/rgywlsqe")
-    BASE.metadata.bind = engine  # Make sure this line is properly indented
-    return sessionmaker(bind=engine)()
+DB_URI = "postgresql://rgywlsqe:lSnciWStimBbJNt5d7EKltRLvFBmAzgA@tyke.db.elephantsql.com/rgywlsqe"
+try:
+    engine = create_engine(DB_URI)
+    connection = engine.connect()
+    print("Database connection successful!")
+    connection.close()
 
 
 BASE = declarative_base()
